84e251389e9813cc6b732bb13b346fd93db09b8c
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-06 13:12:28 +0300

    Update the footer and finisht the home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,20 +1,16 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <footer className="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500">
+    <footer className="mt-20 px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500">
       <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
         <div className="md:max-w-96">
-          <img
-            className="h-9"
-            src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoDark.svg"
-            alt="dummyLogoDark"
-          />
+          <img className="h-9" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            Experience the power of AI with QuickAi. <br />
+            Transform your content creation with our suite of premium AI tools.
+            Write articles, generate images, and enhance your workflow.
           </p>
         </div>
         <div className="flex-1 flex items-start md:justify-end gap-20">
@@ -50,7 +46,7 @@ const Footer = () => {
                   type="email"
                   placeholder="Enter your email"
                 />
-                <button className="bg-blue-600 w-24 h-9 text-white rounded">
+                <button className="bg-primary cursor-pointer w-24 h-9 text-white rounded">
                   Subscribe
                 </button>
               </div>
@@ -59,8 +55,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="pt-4 text-center text-xs md:text-sm pb-5">
-        Copyright 2024 © <a href="https://prebuiltui.com">PrebuiltUI</a>. All
-        Right Reserved.
+        Copyright 2025 © Natnael Tamiru. All Right Reserved.
       </p>
     </footer>
   );
