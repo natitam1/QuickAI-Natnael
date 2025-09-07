cf11b8f84b26723a4768d64fd9eb44a5038ffaa6
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-08 01:02:33 +0300

    Make the creations expandable

--- client/src/components/CreationItem.jsx
+++ client/src/components/CreationItem.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { use, useState } from "react";
 
 const CreationItem = ({ item }) => {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div className="p-4 max-w-5xl text-xs bg-white border border-gray-200 rounded-lg cursor-pointer">
+    <div
+      onClick={() => setExpanded(!expanded)}
+      className="p-4 max-w-5xl text-xs bg-white border border-gray-200 rounded-lg cursor-pointer"
+    >
       <div className="flex justify-between items-center gap-4">
         <div>
           <h2>{item.prompt}</h2>
@@ -14,6 +18,23 @@ const CreationItem = ({ item }) => {
           {item.type}
         </button>
       </div>
+      {expanded && (
+        <div>
+          {item.type === "image" ? (
+            <div>
+              <img
+                src={item.content}
+                alt="image"
+                className="mt-3 w-full max-w-md"
+              />
+            </div>
+          ) : (
+            <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-700">
+              <div>{item.content}</div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
