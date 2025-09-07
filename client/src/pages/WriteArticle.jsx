164803a1e45841068870a0b2502ac56b07c9fbb5
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-08 02:15:39 +0300

    Edit the right col of the article generation

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -55,6 +55,18 @@ const WriteArticle = () => {
           Generate article
         </button>
       </form>
+      {/* Right col */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
+        <div className="flex items-center gap-3">
+          <Edit className="w-5 h-5 text-[#4a7aff]" />
+          <h1 className="text-xl font-semibold">Generated article</h1>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Edit className="w-9 h-9" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
