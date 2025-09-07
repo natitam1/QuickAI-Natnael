baae75feea485bacf11577744db7e1983a36d502
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-07 16:37:38 +0300

    Set up the the dashboard

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { Menu, X } from "lucide-react";
 
 const Layout = () => {
+  const navigate = useNavigate();
+  const [sidebar, setSidebar] = useState(false);
   return (
-    <div>
-      Layout
+    <div className="flex flex-col items-start justify-start h-screen">
+      <nav>
+        <img src={assets.logo} alt="" onClick={() => navigate("/")} />
+        {sidebar ? (
+          <X className="w-6 h-6 text-gray-600 sm:hidden" />
+        ) : (
+          <Menu className="w-6 h-6 text-gray-600 sm:hidden" />
+        )}
+      </nav>
       <Outlet />
     </div>
   );
