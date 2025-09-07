8f2e768de98da0efbec908402e03b571f3180fca
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-08 02:16:41 +0300

    Finish the write articles page

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -64,6 +64,7 @@ const WriteArticle = () => {
         <div className="flex-1 flex justify-center items-center">
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
             <Edit className="w-9 h-9" />
+            <p>Enter a topic and click "Generate article" to get started</p>
           </div>
         </div>
       </div>
